ebeffb9a10d8ada43a73dfa7b6c0b924f224acd5
Puig Molero <adria.puig-molero@sogeti.com>
2022-03-07 08:56:09 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<template>    
+<div id="app">      
+<Users />    
+</div>  
+</template>    
+<script>  
+import Users from './components/Users.vue' 
+export default {    
+name: 'app',    
+components: {      
+Users    
+}  
+}  
+</script>    
+<style>  
+#app {    
+font-family: 'Avenir', Helvetica, Arial, sans-serif;    
+-webkit-font-smoothing: antialiased;    
+-moz-osx-font-smoothing: grayscale;    
+text-align: center;    
+color: #2c3e50;    
+margin-top: 60px;  
+}  
 </style>
